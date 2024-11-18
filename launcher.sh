7aaadb041a589409875debf0a0842b8206bdce88
Olivier van Helden <olivier@van-helden.net>
2024-11-18 15:59:42 -0400

    exit with error code if virtual environment is not properly activated (e.g. when the project location has changed since it was built)

--- launcher.sh
+++ launcher.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
 basedir=$(dirname "$0")
-venv_dir=$(realpath "$basedir/venv")
-timestamp_file="$venv_dir/.last_checked"
 PGM=$(basename "$0")
-venv_dir=venv
+venv_dir=./venv
 
-# DEBUG=true
+for arg in "$@"; do
+    case "$arg" in
+        -v|--verbose) DEBUG=true ;;
+    esac
+done
 
 log() {
     [ "$DEBUG" = "true" ] || return
     echo "$PGM: $*" >&2
 }
 
-log "Activate virtual environment"
+end() {
+    # if first parameter is a number, use it as exit code
+    if [ "$1" -eq "$1" ] 2>/dev/null; then
+        exit_code=$1
+        [ $exit_code -eq 0 ] || DEBUG=true
+        shift
+    else
+        exit_code=0
+    fi
+
+    # if virtual  environment is active, deactivate it
+    [ -n "$VIRTUAL_ENV" ] && log "Deactivating virtual environment" && deactivate
+
+    # user remaining parameters as error message if any, or error # if message is empty
+    if [ -n "$1" ]; then
+        log "Error $exit_code: $*"
+    else
+        log "Error $exit_code"
+    fi
+
+    # Désactiver l'environnement virtuel
+
+    exit $exit_code
+}
+
+log "Activating virtual environment"
+# make sure ven_dir is an absolute path
+venv_dir="$(cd "$(dirname "$venv_dir")" && pwd)/$(basename "$venv_dir")" || exit $?
+timestamp_file="$venv_dir/.last_checked"
+
 # Détecter le système d'exploitation
 OS="$(uname -s)"
 case "$OS" in
@@ -23,40 +54,22 @@ case "$OS" in
     *)          OS="UNKNOWN"
 esac
 
-end() {
-    DEBUG=true
-    # if first parameter is a number, use it as exit code
-    if [ "$1" -eq "$1" ] 2>/dev/null; then
-        exit_code=$1
-        shift
-    else
-        exit_code=0
-    fi
-    # user remaining parameters as error message if any, or error # if message is empty
-    if [ -n "$1" ]; then
-        log "Error $exit_code: $*"
-    else
-        log "Error $exit_code"
-    fi
-
-    log "Deactivating virtual environment"
-    # Désactiver l'environnement virtuel
-    deactivate
-
-    exit $exit_code
-}
-
 # Créer un environnement virtuel s'il n'existe pas
 if [ ! -d "$venv_dir" ]; then
-    if [ "$OS" = "Mac" ]; then
-        python3 -m venv "$venv_dir" --system-site-packages
-    else
+    # if [ "$OS" = "Mac" ]; then
+    #     python3 -m venv "$venv_dir" --system-site-packages
+    # else
         python3 -m venv "$venv_dir"
-    fi
+    # fi
 fi
 
 # Activer l'environnement virtuel
 source "$venv_dir/bin/activate" || end $? "Failed to activate virtual environment in $venv_dir"
+if [[ "$(which python)" != "$venv_dir/bin/python" ]]; then
+  end 1 "Virtual environement is active but Python binary $(which python)
+  is not within virtual environment $venv_dir.
+Delete the venv directory and run the script again."
+fi
 
 # Vérifier les dépendances si elles n'ont pas été vérifiées depuis plus de 6 heures
 if [ ! -f "$timestamp_file" ] || [ $(find "$timestamp_file" -mmin +360) ]; then
