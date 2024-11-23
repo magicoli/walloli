fb2de08038e2d620593d54159bb8494f2b59fee8
Olivier van Helden <olivier@van-helden.net>
2024-11-23 19:02:08 -0400

    v0.1.2-dev * new settings page * new build script for macOS * fix caffeinate staying open when program ends * fix dock icon not working * fix empty space when requested number of players greater than available videos * dev reorganise config read/override process and files * dev move args and config read logic in Settings class * dev use logger module for logging

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ OPTIONS = {
     'iconfile': 'assets/icons/app_icon.icns',
     'plist': {
         'CFBundleName': 'WallOli',
-        'CFBundleShortVersionString': '0.1.1-dev',
-        'CFBundleVersion': '0.1.1-dev',
+        'CFBundleShortVersionString': '0.1.2-dev',
+        'CFBundleVersion': '0.1.2-dev',
         'NSHighResolutionCapable': True,
     },
 }
@@ -31,7 +31,7 @@ setup(
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
     name="WallOli",
-    version="0.1.1-dev",
+    version="0.1.2-dev",
     author="Olivier van Helden",
     author_email="olivier@van-helden.net",
     description="A cross-platform video wall player",
